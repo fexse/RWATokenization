4c6ed5a1c1f53aae70ea75a740f226229dd3e398
drnfexse <duran@fexse.com>
2025-03-11 10:55:20 +0300

    Add files via upload

---
+++ hardhat.config.ts
@@ -0,0 +1,73 @@
+import { HardhatUserConfig } from "hardhat/config";
+//import "@nomiclabs/hardhat-etherscan";
+import "@nomicfoundation/hardhat-verify";
+import "@nomicfoundation/hardhat-toolbox";
+import "@typechain/hardhat";
+import * as dotenv from "dotenv";
+import path from "path";
+
+// .env dosyasındaki değerleri içe aktarır
+dotenv.config();
+
+const RPC_URL = process.env.RPC_URL || "";
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "";
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "";
+
+const config: HardhatUserConfig = {
+  gasReporter: {
+    enabled: true,
+  },
+  solidity: {
+    version: "0.8.24",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+      evmVersion: "shanghai",  // Specify the EVM version for Cancun
+    },
+  },
+  // typechain: {
+  //   outDir: "typechain", // Oluşturulacak tür dosyalarının klasörü
+  //   target: "ethers-v5", // Ethers.js için türleri oluştur
+  // },
+  defaultNetwork: "hardhat",
+  networks: {
+    // hardhat: {
+    //   forking: {
+    //     url: RPC_URL || "",        
+    //     blockNumber: 21671501,// arb: 278070393 // sepolia: 7468704 // eth 21671501
+    //   },
+    //   accounts: {
+    //     count: 32,
+    //   },
+    // },
+    eth: {
+      url: process.env.RPC_URL, // Your eth RPC URL
+      accounts: [process.env.PRIVATE_KEY!], // Your wallet private key
+    },
+    // sepolia: {
+    //   url: process.env.RPC_URL, // Your Sepolia RPC URL
+    //   accounts: [process.env.PRIVATE_KEY!], // Your wallet private key
+    // },
+
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY, // Your Etherscan API Key
+  },
+  // sourcify: {
+  //   // Disabled by default
+  //   // Doesn't need an API key
+  //   enabled: true
+  // },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts",
+  },
+};
+
+export default config;
+
+
