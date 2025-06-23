070ad3f213dd52a2c825841cff0a3dd0ebf5f61b
dermenoguz <dermenoguz@gmail.com>
2025-06-23 14:53:07 +0300

    description check

--- contracts/token/FexseTokenVesting.sol
+++ contracts/token/FexseTokenVesting.sol
@@ -1,11 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import "../ERC20/ERC20.sol";
-import "../ERC20/utils/SafeERC20.sol";
-import "../../utils/AccessControl.sol";
-import "../../utils/ReentrancyGuard.sol";
-import "../../utils/Ownable.sol";
+import "../token/ERC20/ERC20.sol";
+import "../token/ERC20/utils/SafeERC20.sol";
+import "../utils/AccessControl.sol";
+import "../utils/ReentrancyGuard.sol";
+import "../utils/Ownable.sol";
 
 /**
  * @title FexseTokenVesting
